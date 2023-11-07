d41e40914554849f7d717fef6aa0d7fa803b46ad
Gordon Lin <75815453+hallovera@users.noreply.github.com>
2023-11-06 22:43:52 -0500

    Remove currently unused code from NewSidebar.js

--- frontend/src/Components/NewSidebar.js
+++ frontend/src/Components/NewSidebar.js
@@ -1,40 +1,21 @@
 import * as React from "react"
 import GlobalStyles from "@mui/joy/GlobalStyles"
-import Avatar from "@mui/joy/Avatar"
+
 import Box from "@mui/joy/Box"
-import Button from "@mui/joy/Button"
-import Card from "@mui/joy/Card"
-import Chip from "@mui/joy/Chip"
+
 import Divider from "@mui/joy/Divider"
-import IconButton from "@mui/joy/IconButton"
-import Input from "@mui/joy/Input"
-import LinearProgress from "@mui/joy/LinearProgress"
+
 import List from "@mui/joy/List"
 import ListItem from "@mui/joy/ListItem"
 import ListItemButton, { listItemButtonClasses } from "@mui/joy/ListItemButton"
 import ListItemContent from "@mui/joy/ListItemContent"
 import Typography from "@mui/joy/Typography"
 import Sheet from "@mui/joy/Sheet"
-import Stack from "@mui/joy/Stack"
-import SearchRoundedIcon from "@mui/icons-material/SearchRounded"
-import HomeRoundedIcon from "@mui/icons-material/HomeRounded"
 import DashboardRoundedIcon from "@mui/icons-material/DashboardRounded"
-import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded"
-import AssignmentRoundedIcon from "@mui/icons-material/AssignmentRounded"
-import QuestionAnswerRoundedIcon from "@mui/icons-material/QuestionAnswerRounded"
-import GroupRoundedIcon from "@mui/icons-material/GroupRounded"
-import SupportRoundedIcon from "@mui/icons-material/SupportRounded"
-import SettingsRoundedIcon from "@mui/icons-material/SettingsRounded"
-import CloseRoundedIcon from "@mui/icons-material/CloseRounded"
-import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded"
-import BrightnessAutoRoundedIcon from "@mui/icons-material/BrightnessAutoRounded"
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown"
 
 import HelpOutlineRoundedIcon from '@mui/icons-material/HelpOutlineRounded';
 import InsertChartRoundedIcon from '@mui/icons-material/InsertChartRounded';
 import OpenInNewRoundedIcon from '@mui/icons-material/OpenInNewRounded';
-
-// import ColorSchemeToggle from "./ColorSchemeToggle"
 
 import {ReactComponent as Logo} from "../LOGO-SoC-RGB.svg";
 
@@ -182,142 +163,7 @@ export default function NewSidebar() {
               </ListItemContent>
             </ListItemButton>
           </ListItem>
-
-          {/* <ListItem nested>
-            <Toggler
-              renderToggle={({ open, setOpen }) => (
-                <ListItemButton onClick={() => setOpen(!open)}>
-                  <AssignmentRoundedIcon />
-                  <ListItemContent>
-                    <Typography level="title-sm">Tasks</Typography>
-                  </ListItemContent>
-                  <KeyboardArrowDownIcon
-                    sx={{ transform: open ? "rotate(180deg)" : "none" }}
-                  />
-                </ListItemButton>
-              )}
-            >
-              <List sx={{ gap: 0.5 }}>
-                <ListItem sx={{ mt: 0.5 }}>
-                  <ListItemButton>All tasks</ListItemButton>
-                </ListItem>
-                <ListItem>
-                  <ListItemButton>Backlog</ListItemButton>
-                </ListItem>
-                <ListItem>
-                  <ListItemButton>In progress</ListItemButton>
-                </ListItem>
-                <ListItem>
-                  <ListItemButton>Done</ListItemButton>
-                </ListItem>
-              </List>
-            </Toggler>
-          </ListItem>
-
-          <ListItem>
-            <ListItemButton
-              role="menuitem"
-              component="a"
-              href="/joy-ui/getting-started/templates/messages/"
-            >
-              <QuestionAnswerRoundedIcon />
-              <ListItemContent>
-                <Typography level="title-sm">Messages</Typography>
-              </ListItemContent>
-              <Chip size="sm" color="primary" variant="solid">
-                4
-              </Chip>
-            </ListItemButton>
-          </ListItem>
-
-          <ListItem nested>
-            <Toggler
-              renderToggle={({ open, setOpen }) => (
-                <ListItemButton onClick={() => setOpen(!open)}>
-                  <GroupRoundedIcon />
-                  <ListItemContent>
-                    <Typography level="title-sm">Users</Typography>
-                  </ListItemContent>
-                  <KeyboardArrowDownIcon
-                    sx={{ transform: open ? "rotate(180deg)" : "none" }}
-                  />
-                </ListItemButton>
-              )}
-            >
-              <List sx={{ gap: 0.5 }}>
-                <ListItem sx={{ mt: 0.5 }}>
-                  <ListItemButton
-                    role="menuitem"
-                    component="a"
-                    href="/joy-ui/getting-started/templates/profile-dashboard/"
-                  >
-                    My profile
-                  </ListItemButton>
-                </ListItem>
-                <ListItem>
-                  <ListItemButton>Create a new user</ListItemButton>
-                </ListItem>
-                <ListItem>
-                  <ListItemButton>Roles & permission</ListItemButton>
-                </ListItem>
-              </List>
-            </Toggler>
-          </ListItem> */}
         </List>
-
-        {/* <List
-          size="sm"
-          sx={{
-            mt: "auto",
-            flexGrow: 0,
-            "--ListItem-radius": theme => theme.vars.radius.sm,
-            "--List-gap": "8px",
-            mb: 2
-          }}
-        >
-          <ListItem>
-            <ListItemButton>
-              <SupportRoundedIcon />
-              Support
-            </ListItemButton>
-          </ListItem>
-          <ListItem>
-            <ListItemButton>
-              <SettingsRoundedIcon />
-              Settings
-            </ListItemButton>
-          </ListItem>
-        </List> */}
-        {/* <Card
-          invertedColors
-          variant="soft"
-          color="warning"
-          size="sm"
-          sx={{ boxShadow: "none" }}
-        >
-          <Stack
-            direction="row"
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <Typography level="title-sm">Used space</Typography>
-            <IconButton size="sm">
-              <CloseRoundedIcon />
-            </IconButton>
-          </Stack>
-          <Typography level="body-xs">
-            Your team has used 80% of your available space. Need more?
-          </Typography>
-          <LinearProgress
-            variant="outlined"
-            value={80}
-            determinate
-            sx={{ my: 1 }}
-          />
-          <Button size="sm" variant="solid">
-            Upgrade plan
-          </Button>
-        </Card> */}
       </Box>
       <Divider />
       <Box
@@ -364,21 +210,6 @@ export default function NewSidebar() {
           </ListItem>
         </List>
       </Box>
-      {/* <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
-        
-        <Avatar
-          variant="outlined"
-          size="sm"
-          src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286"
-        />
-        <Box sx={{ minWidth: 0, flex: 1 }}>
-          <Typography level="title-sm">Siriwat K.</Typography>
-          <Typography level="body-xs">siriwatk@test.com</Typography>
-        </Box>
-        <IconButton size="sm" variant="plain" color="neutral">
-          <LogoutRoundedIcon />
-        </IconButton>
-      </Box> */}
     </Sheet>
   )
 }
